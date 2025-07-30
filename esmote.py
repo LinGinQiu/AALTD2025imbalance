478a471fb4e6550d7b50e0e5e4073b0ea53cc264
Chris Qiu <chuanhangqiu@outlook.com>
2025-07-30 16:01:43 +0100

    simplify

--- esmote.py
+++ esmote.py
@@ -1,37 +1,132 @@
 from collections import OrderedDict
 from typing import Optional, Union
+
 import numpy as np
 from numba import prange
 from sklearn.utils import check_random_state
 
-from esmote._wip.rt.classification.distance_based import KNeighborsTimeSeriesClassifier
-from esmote._wip.rt.clustering.averaging._ba_utils import _get_alignment_path
+from aeon.classification.distance_based import KNeighborsTimeSeriesClassifier
+from aeon.clustering.averaging._ba_utils import _get_alignment_path
 from aeon.transformations.collection import BaseCollectionTransformer
-from esmote._wip.rt.utils._threading import threaded
 
 __all__ = ["ESMOTE"]
 
+
+class KNN(KNeighborsTimeSeriesClassifier):
+    """
+    KNN classifier for time series data, adapted to work with ESMOTE.
+    This class is a wrapper around the original KNeighborsTimeSeriesClassifier
+    to ensure compatibility with the ESMOTE transformation.
+    """
+
+    def _fit_setup(self, X, y):
+        # KNN can support if all labels are the same so always return False for single
+        # class problem so the fit will always run
+        X, y, _ = super()._fit_setup(X, y)
+        return X, y, False
+
+    def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
+        """Find the K-neighbors of a point.
+
+        Returns indices of and distances to the neighbors of each point.
+
+        Parameters
+        ----------
+        X : 3D np.ndarray of shape = (n_cases, n_channels, n_timepoints) or list of
+        shape [n_cases] of 2D arrays shape (n_channels,n_timepoints_i)
+            The query point or points.
+            If not provided, neighbors of each indexed point are returned.
+            In this case, the query point is not considered its own neighbor.
+        n_neighbors : int, default=None
+            Number of neighbors required for each sample. The default is the value
+            passed to the constructor.
+        return_distance : bool, default=True
+            Whether or not to return the distances.
+
+        Returns
+        -------
+        neigh_dist : ndarray of shape (n_queries, n_neighbors)
+            Array representing the distances to points, only present if
+            return_distance=True.
+        neigh_ind : ndarray of shape (n_queries, n_neighbors)
+            Indices of the nearest points in the population matrix.
+        """
+        self._check_is_fitted()
+        import numbers
+
+        from aeon.distances import pairwise_distance
+
+        if n_neighbors is None:
+            n_neighbors = self.n_neighbors
+        elif n_neighbors <= 0:
+            raise ValueError(f"Expected n_neighbors > 0. Got {n_neighbors}")
+        elif not isinstance(n_neighbors, numbers.Integral):
+            raise TypeError(
+                f"n_neighbors does not take {type(n_neighbors)} value, "
+                "enter integer value"
+            )
+
+        query_is_train = X is None
+        if query_is_train:
+            X = self.X_
+            n_neighbors += 1
+        else:
+            X = self._preprocess_collection(X, store_metadata=False)
+            self._check_shape(X)
+
+        # Compute pairwise distances between X and fit data
+        distances = pairwise_distance(
+            X,
+            self.X_ if not query_is_train else None,
+            method=self.distance,
+            **self._distance_params,
+        )
+
+        sample_range = np.arange(distances.shape[0])[:, None]
+        neigh_ind = np.argpartition(distances, n_neighbors - 1, axis=1)
+        neigh_ind = neigh_ind[:, :n_neighbors]
+        neigh_ind = neigh_ind[
+            sample_range, np.argsort(distances[sample_range, neigh_ind])
+        ]
+
+        if query_is_train:
+            neigh_ind = neigh_ind[:, 1:]
+
+        if return_distance:
+            if query_is_train:
+                neigh_dist = distances[sample_range, neigh_ind]
+                return neigh_dist, neigh_ind
+            return distances[sample_range, neigh_ind], neigh_ind
+
+        return neigh_ind
+
+
 class ESMOTE(BaseCollectionTransformer):
     """
-    Over-sampling using the Synthetic Minority Over-sampling TEchnique (SMOTE)[1]_.
-    An adaptation of the imbalance-learn implementation of SMOTE in
-    imblearn.over_sampling.SMOTE. sampling_strategy is sampling target by
-    targeting all classes but not the majority, which is directly expressed in
-    _fit.sampling_strategy.
+    Elastic Synthetic Minority Over-sampling Technique (ESMOTE).
+
     Parameters
     ----------
     n_neighbors : int, default=5
         The number  of nearest neighbors used to define the neighborhood of samples
         to use to generate the synthetic time series.
-        `~sklearn.neighbors.NearestNeighbors` instance will be fitted in this case.
+    distance : str or callable, default="msm"
+        The distance metric to use for the nearest neighbors search and alignment path
+        of synthetic time series.
+    weights : str or callable, default = 'uniform'
+        Mechanism for weighting a vote one of: ``'uniform'``, ``'distance'``,
+        or a callable
+        function.
     random_state : int, RandomState instance or None, default=None
         If `int`, random_state is the seed used by the random number generator;
         If `RandomState` instance, random_state is the random number generator;
         If `None`, the random number generator is the `RandomState` instance used
         by `np.random`.
+
     See Also
     --------
     ADASYN
+
     References
     ----------
     .. [1] Chawla et al. SMOTE: synthetic minority over-sampling technique, Journal
@@ -40,7 +135,7 @@ class ESMOTE(BaseCollectionTransformer):
     """
 
     _tags = {
-        "capability:multivariate": True,
+        "capability:multivariate": False,
         "capability:unequal_length": False,
         "requires_y": True,
     }
@@ -48,7 +143,7 @@ class ESMOTE(BaseCollectionTransformer):
     def __init__(
         self,
         n_neighbors=5,
-        distance: Union[str, callable] = "euclidean",
+        distance: Union[str, callable] = "msm",
         distance_params: Optional[dict] = None,
         weights: Union[str, callable] = "uniform",
         n_jobs: int = 1,
@@ -69,7 +164,7 @@ class ESMOTE(BaseCollectionTransformer):
 
     def _fit(self, X, y=None):
         self._random_state = check_random_state(self.random_state)
-        self.nn_ = KNeighborsTimeSeriesClassifier(
+        self.nn_ = KNN(
             n_neighbors=self.n_neighbors + 1,
             distance=self.distance,
             distance_params=self._distance_params,
@@ -91,14 +186,6 @@ class ESMOTE(BaseCollectionTransformer):
         return self
 
     def _transform(self, X, y=None):
-        # if input is 3D (n, c, l) or 2D (n, l), proceed; otherwise fall back to original
-        if X.ndim == 3:
-            n_samples, n_channels, seq_len = X.shape
-        elif X.ndim == 2:
-            X = X[:, np.newaxis, :]
-            n_samples, n_channels, seq_len = X.shape
-        else:
-            raise ValueError("Input X must be 3D or 2D: (n_samples, (n_channels), seq_len)")
         X_resampled = [X.copy()]
         y_resampled = [y.copy()]
 
@@ -124,11 +211,11 @@ class ESMOTE(BaseCollectionTransformer):
             )
             X_resampled.append(X_new)
             y_resampled.append(y_new)
-        X_resampled = np.vstack(X_resampled)
-        y_resampled = np.hstack(y_resampled)
-        return X_resampled, y_resampled
+        X_synthetic = np.vstack(X_resampled)
+        y_synthetic = np.hstack(y_resampled)
 
-    @threaded
+        return X_synthetic, y_synthetic
+
     def _make_samples(
         self, X, y_dtype, y_type, nn_data, nn_num, n_samples, step_size=1.0, n_jobs=1
     ):
@@ -136,9 +223,13 @@ class ESMOTE(BaseCollectionTransformer):
             low=0, high=nn_num.size, size=n_samples
         )
 
-        steps = step_size * self._random_state.uniform(size=n_samples)[:, np.newaxis]
+        steps = (
+            step_size
+            * self._random_state.uniform(low=0, high=1, size=n_samples)[:, np.newaxis]
+        )
         rows = np.floor_divide(samples_indices, nn_num.shape[1])
         cols = np.mod(samples_indices, nn_num.shape[1])
+        distance = self.distance
 
         X_new = _generate_samples(
             X,
@@ -148,14 +239,13 @@ class ESMOTE(BaseCollectionTransformer):
             cols,
             steps,
             random_state=self._random_state,
-            distance=self.distance,
+            distance=distance,
             **self._distance_params,
         )
         y_new = np.full(n_samples, fill_value=y_type, dtype=y_dtype)
         return X_new, y_new
 
 
-# @njit(cache=True, fastmath=True, parallel=True)
 def _generate_samples(
     X,
     nn_data,
@@ -186,10 +276,8 @@ def _generate_samples(
         i = rows[count]
         j = cols[count]
         curr_ts = X[i]  # shape: (c, l)
-        nn_ts = nn_data[nn_num[i, j]] # shape: (c, l)
+        nn_ts = nn_data[nn_num[i, j]]  # shape: (c, l)
         new_ts = curr_ts.copy()
-
-        c = random_state.uniform(0.5, 2.0)  # Randomize MSM penalty parameter
         alignment, _ = _get_alignment_path(
             nn_ts,
             curr_ts,
@@ -213,29 +301,34 @@ def _generate_samples(
             path_list[k].append(l)
 
         # num_of_alignments = np.zeros_like(curr_ts, dtype=np.int32)
-        empty_of_array = np.zeros_like(curr_ts, dtype=type(curr_ts[0])) # shape: (c, l)
+        empty_of_array = np.zeros_like(curr_ts, dtype=float)  # shape: (c, l)
 
         for k, l in enumerate(path_list):
             if len(l) == 0:
-                raise ValueError("No alignment found")
+                print("No alignment found for time step")
+                return new_ts
+
             key = random_state.choice(l)
             # Compute difference for all channels at this time step
             empty_of_array[:, k] = curr_ts[:, k] - nn_ts[:, key]
 
-        X_new[count]  = new_ts + steps[count] * empty_of_array  #/ num_of_alignments
+        X_new[count] = new_ts + steps[count] * empty_of_array  # / num_of_alignments
 
     return X_new
 
 
-
 if __name__ == "__main__":
     # Example usage
-    X = np.random.randn(100, 3, 100)
-    y = np.random.choice([0, 0, 1], size=100)
+    n_samples = 100  # Total number of labels
+    majority_num = 90  # number of majority class
+    minority_num = n_samples - majority_num  # number of minority class
+    np.random.seed(42)
+
+    X = np.random.rand(n_samples, 1, 10)
+    y = np.array([0] * majority_num + [1] * minority_num)
     print(np.unique(y, return_counts=True))
     smote = ESMOTE(n_neighbors=5, random_state=1, distance="msm")
 
     X_resampled, y_resampled = smote.fit_transform(X, y)
     print(X_resampled.shape)
     print(np.unique(y_resampled,return_counts=True))
-    stop = ""
--- esmote/_wip/rt/transformations/collection/imbalance/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Supervised transformers to rebalance colelctions of time series."""
-
-__all__ = ["ADASYN", "SMOTE", "OHIT","TSMOTE", "ESMOTE"]
